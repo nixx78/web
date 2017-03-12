c7e1527ae1c661af2555bb936d2dd0cd97e05fe3
nixx78 <nixx78@gmail.com>
2017-03-12 17:48:02 +0200

    Configuration changed from XML to Java

--- ws-poc/src/main/java/lv/nixx/poc/ws/client/App.java
+++ ws-poc/src/main/java/lv/nixx/poc/ws/client/App.java
@@ -3,7 +3,7 @@ package lv.nixx.poc.ws.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import lv.nixx.poc.wsdl.currency.Currency;
 
@@ -13,7 +13,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 		CurrencyService currencyService = context.getBean(CurrencyService.class);
 
 		Currency fromCurrency = Currency.USD;
@@ -21,5 +21,6 @@ public class App {
 		Double conversionRate = currencyService.getConversionRate(fromCurrency, toCurrency);
 
 		log.info(String.format("The conversion rate from %s to %s is %s.", fromCurrency, toCurrency, conversionRate));
+		
 	}
 }
--- ws-poc/src/main/java/lv/nixx/poc/ws/client/ApplicationConfig.java
+++ ws-poc/src/main/java/lv/nixx/poc/ws/client/ApplicationConfig.java
@@ -1,10 +1,37 @@
 package lv.nixx.poc.ws.client;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.SoapVersion;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 
 @Configuration
+@ComponentScan(basePackages = "lv.nixx.poc.ws.client")
 public class ApplicationConfig {
 	
+	@Bean
+	public SaajSoapMessageFactory soapMessageFactory() {
+		SaajSoapMessageFactory f = new SaajSoapMessageFactory();
+		f.setSoapVersion(SoapVersion.SOAP_12);
+		return f;
+	}
 	
-
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller m = new Jaxb2Marshaller();
+		m.setContextPath("lv.nixx.poc.wsdl.currency");
+		return m;
+	}
+	
+	@Bean
+	public WebServiceTemplate webServiceTemplate() {
+		WebServiceTemplate wst = new WebServiceTemplate(soapMessageFactory());
+		wst.setMarshaller(marshaller());
+		wst.setUnmarshaller(marshaller());
+		wst.setDefaultUri("http://www.webservicex.net/CurrencyConvertor.asmx?WSDL");
+		return wst;
+	}
 }
