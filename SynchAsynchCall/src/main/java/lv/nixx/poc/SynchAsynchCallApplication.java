d4aa27741fdf07ea5e08a89d6a067973c992149c
nixx78 <nixx78@gmail.com>
2017-04-27 20:31:47 +0300

    Initial project commit

---
+++ SynchAsynchCall/src/main/java/lv/nixx/poc/SynchAsynchCallApplication.java
@@ -0,0 +1,12 @@
+package lv.nixx.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchAsynchCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchAsynchCallApplication.class, args);
+	}
+}
---
+++ SynchAsynchCall/src/test/java/lv/nixx/poc/SynchAsynchBlockingQueue.java
@@ -0,0 +1,114 @@
+package lv.nixx.poc;
+
+import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.Test;
+
+public class SynchAsynchBlockingQueue {
+
+	AtomicInteger idProvider = new AtomicInteger(0);
+
+	@Test
+	public void executeProducerConsumer() throws InterruptedException {
+		BlockingQueue<RequestResponse> bq = new ArrayBlockingQueue<>(10, true);
+
+		Consumer consumer = new Consumer(bq);
+		
+		Arrays.asList(
+				new RequestProducer(bq, "1"),
+				new RequestProducer(bq, "2"),
+				new RequestProducer(bq, "3")
+				).forEach(RequestProducer::start);
+
+		consumer.start();
+		consumer.join();
+	}
+
+	class RequestProducer extends Thread {
+		private static final long TIMEOUT = 5000;
+		BlockingQueue<RequestResponse> bq;
+		String threadPostfix;
+
+		RequestProducer(BlockingQueue<RequestResponse> bq, String threadPostfix) {
+			super("ProducerThread." + threadPostfix);
+			this.bq = bq;
+			this.threadPostfix = threadPostfix;
+		}
+
+		@Override
+		public void run() {
+			Thread currentThread = Thread.currentThread();
+			while(!currentThread.isInterrupted()) {
+				int id = idProvider.getAndIncrement();
+				RequestResponse r = new RequestResponse(id, "req." + System.currentTimeMillis(), null);
+				if ( bq.offer(r) ) {
+					System.out.println("Thread [" + currentThread.getName() + "] send [" + r + "]");
+					synchronized (r) {
+						try {
+							r.wait(TIMEOUT);
+							
+							System.out.println("Thread [" + currentThread.getName() + "] response [" + r + "]");
+						} catch (InterruptedException e) {
+							currentThread.interrupt();
+						}
+					}
+				} else {
+					System.out.println("Request for ID [" + id + "] can't be processed, queue is full");
+				}
+			}
+		}
+
+	}
+
+	class Consumer extends Thread {
+
+		BlockingQueue<RequestResponse> bq;
+
+		public Consumer(BlockingQueue<RequestResponse> bq) {
+			super("ConsumerThread");
+			this.bq = bq;
+		}
+
+		@Override
+		public void run() {
+			Thread currentThread = Thread.currentThread();
+			while (!currentThread.isInterrupted()) {
+				try {
+					TimeUnit.SECONDS.sleep(1);
+					RequestResponse r = bq.take();
+					System.out.println("Thread [" + currentThread.getName() + "] processed [" + r + "]");
+					synchronized (r) {
+						r.response = r.request + ".resp";
+						r.notify();
+					}
+				} catch (InterruptedException e) {
+					currentThread.interrupt();
+				}
+			}
+		}
+
+	}
+
+	class RequestResponse {
+		int id;
+		String request;
+		String response;
+
+		public RequestResponse(int id, String request, String response) {
+			this.id = id;
+			this.request = request;
+			this.response = response;
+		}
+
+		@Override
+		public String toString() {
+			return "RequestResponse [id=" + id + ", request=" + request + ", response=" + response + "]";
+		}
+
+	}
+
+}
