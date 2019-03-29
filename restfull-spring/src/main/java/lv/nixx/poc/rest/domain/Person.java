9c7dec059a12de45cb2ea3aec5ec2bba76315618
nixx78 <nixx78@gmail.com>
2019-03-29 13:41:53 +0200

    Minor changes

--- restfull-spring/src/main/java/lv/nixx/poc/rest/domain/Person.java
+++ restfull-spring/src/main/java/lv/nixx/poc/rest/domain/Person.java
@@ -1,14 +1,17 @@
 package lv.nixx.poc.rest.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.*;
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @ToString
+@NoArgsConstructor
+@Data
 public class Person {
 
 	private int id;
@@ -16,46 +19,11 @@ public class Person {
 	private String surname;
 	private Date dateOfBirth;
 	
-	public Person(){
-	}
-	
 	public Person(String name, String surname, Date dateOfBirth ){
 		this.name = name;
 		this.surname = surname;
 		this.dateOfBirth = dateOfBirth;
 	}
 
-	@XmlAttribute(name="id")
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id){
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getSurname() {
-		return surname;
-	}
-	
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	
-	public Date getDateOfBirth() {
-		return dateOfBirth;
-	}
-	
-	public void setDateOfBirth(Date dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
 
 }
