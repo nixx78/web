94c8af4761aa91736be476dba222856bd1286d43
nixx78 <nixx78@gmail.com>
2020-09-18 14:51:02 +0300

    Project moved to 'data' repo

--- tx-sandbox/src/main/java/lv/nixx/poc/txs/data/BalanceRepository.java
+++
@@ -1,7 +0,0 @@
-package lv.nixx.poc.txs.data;
-
-import lv.nixx.poc.txs.data.model.AccountBalance;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BalanceRepository extends JpaRepository<AccountBalance, Long> {
-}
