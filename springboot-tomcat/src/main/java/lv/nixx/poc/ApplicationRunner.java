27aa6a5c98ad58a78d2fb7c023b9c870add30a15
nixx78 <nixx78@gmail.com>
2021-05-19 12:05:40 +0300

    Experiments how to include additional profile

--- springboot-tomcat/src/main/java/lv/nixx/poc/ApplicationRunner.java
+++ springboot-tomcat/src/main/java/lv/nixx/poc/ApplicationRunner.java
@@ -3,9 +3,19 @@ package lv.nixx.poc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.env.AbstractEnvironment;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
 public class ApplicationRunner extends SpringBootServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "prod");
+        super.onStartup(servletContext);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
