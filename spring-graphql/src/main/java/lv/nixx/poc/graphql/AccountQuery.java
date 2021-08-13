787a3ba2a674cb3ca1979835a7aeddffde30dbd1
nixx78 <nixx78@gmail.com>
2021-08-13 09:40:49 +0300

    JPA Specification to retrieve balance added

--- spring-graphql/src/main/java/lv/nixx/poc/graphql/AccountQuery.java
+++ spring-graphql/src/main/java/lv/nixx/poc/graphql/AccountQuery.java
@@ -1,12 +1,15 @@
 package lv.nixx.poc.graphql;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.DataFetchingFieldSelectionSet;
 import lv.nixx.poc.graphql.domain.Account;
 import lv.nixx.poc.graphql.domain.dto.AccountDTO;
 import lv.nixx.poc.graphql.domain.entity.AccountEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.criteria.JoinType;
 import java.util.Optional;
 
 @Component
@@ -19,9 +22,22 @@ public class AccountQuery implements GraphQLQueryResolver {
         this.repo = accRepo;
     }
 
-    public Account accountById(final long id) {
-        Optional<AccountEntity> byId = repo.findById(id);
-        return new AccountDTO(byId.orElseThrow( () -> new IllegalArgumentException("Account with Id: " + id + " not exists")));
+    public Account accountById(final long id, DataFetchingEnvironment environment) {
+
+        DataFetchingFieldSelectionSet fields = environment.getSelectionSet();
+
+        Optional<AccountEntity> result;
+        if (fields.contains("balance")) {
+            result = repo.findOne((root, query, builder) -> {
+                root.fetch("balance", JoinType.LEFT);
+                return builder.equal(root.get("id"), id);
+            });
+        } else {
+            result = repo.findById(id);
+        }
+
+        return new AccountDTO(result.orElseThrow(() -> new IllegalArgumentException("Account with Id: " + id + " not exists")));
+
     }
 
     /*
--- spring-graphql/src/main/java/lv/nixx/poc/graphql/AccountRepository.java
+++ spring-graphql/src/main/java/lv/nixx/poc/graphql/AccountRepository.java
@@ -1,7 +1,9 @@
 package lv.nixx.poc.graphql;
 
 import lv.nixx.poc.graphql.domain.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AccountRepository extends CrudRepository<AccountEntity, Long> {
+public interface AccountRepository extends CrudRepository<AccountEntity, Long>, JpaSpecificationExecutor<AccountEntity> {
+
 }
