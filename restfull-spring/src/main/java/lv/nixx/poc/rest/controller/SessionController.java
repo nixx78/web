f2741dbd7ee1085e75b42a93e9afa06705659aaf
nixx78 <nixx78@gmail.com>
2021-10-04 08:58:39 +0300

    Logout endpoint added

--- restfull-spring/src/main/java/lv/nixx/poc/rest/controller/SessionController.java
+++ restfull-spring/src/main/java/lv/nixx/poc/rest/controller/SessionController.java
@@ -3,6 +3,7 @@ package lv.nixx.poc.rest.controller;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
 
@@ -15,10 +16,22 @@ public class SessionController {
             "cid2", "Details Name2, Surname2"
     );
 
-    @GetMapping("/login")
+    @GetMapping("/loginWithSession")
     public String login(@RequestParam(name = "customerId") String customerId, @ApiIgnore HttpSession session) {
         session.setAttribute("customerId", customerId);
         return "Customer:" + customerId + " login success";
+    }
+
+    @GetMapping("/logout")
+    public String logout(@ApiIgnore HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String customerId = (String) session.getAttribute("customerId");
+        if (customerId == null) {
+            return "Customer not logged in";
+        }
+
+        session.invalidate();
+        return "Customer [" + customerId + "] logout success";
     }
 
     @GetMapping("/customerDetails")
@@ -31,3 +44,10 @@ public class SessionController {
     }
 
 }
+
+/*
+ * Cookies
+ * Hidden form field
+ * URL Rewriting
+ * HttpSession
+ */
