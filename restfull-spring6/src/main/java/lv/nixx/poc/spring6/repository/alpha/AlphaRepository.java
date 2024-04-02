5390ca8a4aead2034fd10b79a0568d4d235b0896
nixx78 <nixx78@gmail.com>
2024-04-02 11:52:50 +0300

    Projects cleanup

--- restfull-spring6/src/main/java/lv/nixx/poc/spring6/repository/alpha/AlphaRepository.java
+++
@@ -1,9 +0,0 @@
-package lv.nixx.poc.spring6.repository.alpha;
-
-import lv.nixx.poc.spring6.orm.alpha.AlphaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlphaRepository extends JpaRepository<AlphaEntity, Long> {
-}
--- restfull-spring6/src/main/resources/dev-schema/beta-init-data.sql
+++
@@ -1,3 +0,0 @@
-insert into BETA.BETA_TABLE (BETA_ID, BETA_STRING) Values(1, 'B1_DEV');
-insert into BETA.BETA_TABLE (BETA_ID, BETA_STRING) Values(2, 'B2_DEV');
-insert into BETA.BETA_TABLE (BETA_ID, BETA_STRING) Values(3, 'B3_DEV');
